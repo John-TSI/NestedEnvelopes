2b7eb7cabc052820e4521c0ad63955309b7a8217
John Roughley <jroughley@softwareinstitute.com>
2022-08-30 12:09:09 +0100

    corrected the solution

--- src/solution.cpp
+++ src/solution.cpp
@@ -2,7 +2,7 @@
 #include"../inc/solution.hpp"
 
 
-// idea: sort envelopes by decreasing width, then find the longest decreasing subsequence (LDS) of their heights
+// idea: sort envelopes by decreasing width, then find the length of longest decreasing subsequence (LDS) of their heights
 int Solution::maxEnvelopes(std::vector<std::vector<int>>& envelopes)
 {
     std::sort( envelopes.begin(), envelopes.end(), 
@@ -16,11 +16,11 @@ int Solution::maxEnvelopes(std::vector<std::vector<int>>& envelopes)
         if(envelopes[i][0] == maxWidth || envelopes[i][1] == heights.back()){ continue; }  // neither dimension can be equal
         if(envelopes[i][1] > heights.back())  // found an envelope taller than current shortest
         {
-            heights.pop_back();
-            heights.push_back(envelopes[i][1]);
+            // replace the largest height less than current height with current height
+            *std::lower_bound(heights.end()-1, heights.begin(), envelopes[i][1]) = envelopes[i][1];
         }
         else{ heights.push_back(envelopes[i][1]); }  // found a new shortest envelope, append its height to subsequence
     }
-    // 'heights' contains the LDS of heights, its length is largest possible number of nested envelopes
+    // length of 'heights' is largest possible number of nested envelopes
     return heights.size();
 }
